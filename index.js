c34e43236ad65cca0c7fbf3128ec4879d432caa3
Suglo Suleman Rufiatu <101270194+rufia-suleman@users.noreply.github.com>
2022-04-19 07:50:27 -0700

    first commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const morgan = require("morgan")
 const connectDB = require("./config/connectDB")
 const foodRoute = require("./routes/foodRoute")
+const userRouter = require("./routes/userRoute")
 
 require("dotenv").config()
 
@@ -12,6 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(morgan("dev"))
 app.use(foodRoute)
+app.use("/api", userRouter)
 
 
 const PORT = process.env.PORT||9000
